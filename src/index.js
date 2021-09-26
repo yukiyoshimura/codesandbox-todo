89267683a271f074dd4835967e0b5366d82b80b0
yukiyoshimura <yuukiyy0234@gmail.com>
2021-09-26 21:04:58 +0900

    add todo

--- src/index.js
+++ src/index.js
@@ -2,26 +2,69 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 削除ボタンタグ
+const deleteButton = document.createElement("button");
+deleteButton.innerText = "削除";
+deleteButton.addEventListener("click", () => {
+  deleteFromIncompleteList(deleteButton.parentNode);
+});
+
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+const createIncompleteList = (text) => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
   const div = document.createElement("div");
   div.className = "list-row";
 
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // 完了ボタンタグ
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    deleteFromIncompleteList(completeButton.parentNode);
+
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    backButton.addEventListener("click", () => {
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // get text
+      const text = backButton.parentNode.firstElementChild.innerText;
+
+      createIncompleteList(text);
+    });
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // 削除ボタンタグ
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    const deleteTarget = deleteButton.parentNode;
-    console.log(deleteTarget);
-    alert("削除");
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // 子要素を追加
